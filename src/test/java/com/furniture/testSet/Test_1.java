8710e0589562be19ec7af84ef80cad017c928b81
Sumitsingh27 <60751330+Sumitsingh27@users.noreply.github.com>
2020-02-24 17:36:36 +0000

    Update Test_1.java

--- src/test/java/com/furniture/testSet/Test_1.java
+++ src/test/java/com/furniture/testSet/Test_1.java
@@ -28,14 +28,14 @@ public class Test_1 extends BaseTestClass {
 		
 		
 // Register the User
-		//RestAssuredPages.registerUser("test@why123.com", "4352678923", "Newuser", "Test@1234");
+		RestAssuredPages.registerUser("test@why123.com", "4352678923", "Newuser", "Test@1234");
 		RestAssuredPages.home();
-		//dataBaseValidations.validateEmailFromDatabaseViaMobileNumber("4352678923", "test@why123.com");
+		dataBaseValidations.validateEmailFromDatabaseViaMobileNumber("4352678923", "test@why123.com");
 //Using the register User Logging in
 		HomePage.tapOnSignIn(driver);
-//		SignInPage.enterEmail(driver, "test@test221.com");
-//		SignInPage.enterPassword(driver, "Test@1234");
-//		SignInPage.clickOnSignInTab(driver);
+		SignInPage.enterEmail(driver, "test@test221.com");
+		SignInPage.enterPassword(driver, "Test@1234");
+		SignInPage.clickOnSignInTab(driver);
 		
 		
 		log.info("testCase1_Selection End's");
